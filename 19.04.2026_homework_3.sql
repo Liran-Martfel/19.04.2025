cf4ef813b363ccf0d8e80a45492c5305af109557
Liran Martfel <liran.martfel@gmail.com>
2026-04-22 09:28:58 +0300

    Update 19.04.2026_homework_3.sql

--- 19.04.2026_homework_3.sql
+++ 19.04.2026_homework_3.sql
@@ -1,34 +1,40 @@
+--1
 SELECT DISTINCT type
 FROM netflix_movies_detailed_up_to_2025
 ORDER BY type;
 
-SELECT DISTINCT cast
+--2
+SELECT DISTINCT 'cast'
 FROM netflix_movies_detailed_up_to_2025
-WHERE genre LIKE '%Action%'
-AND cast IS NOT NULL
+WHERE type = 'Movies'
+AND genres LIKE '%Action%'
+AND 'cast' IS NOT NULL
 LIMIT 20;
 
+--3
 SELECT 
     title AS movie, 
     rating AS score
 FROM netflix_movies_detailed_up_to_2025
-WHERE genre LIKE '%Action%' 
+WHERE genres LIKE '%Action%' 
   AND release_year > 2015
 ORDER BY rating DESC
 LIMIT 5;
 
+--4
 SELECT title
 FROM netflix_movies_detailed_up_to_2025
-WHERE genre LIKE '%Drama%' 
-  AND cast IS NULL;
-  
+WHERE genres LIKE '%Drama%' 
+  AND 'cast' IS NULL;
+
+--5
 SELECT 
     title AS movie, 
     rating AS score
 FROM netflix_movies_detailed_up_to_2025
 ORDER BY rating DESC;
   
-  
+--6
 SELECT 
     title AS movie, 
     rating AS score
